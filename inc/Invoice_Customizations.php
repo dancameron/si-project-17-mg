8ff3e9d3bab70aeb97f01c2798779c3e577773a7
Dan Cameron <dan@sproutapps.co>
2017-05-18 12:14:12 -0700

    Some string replacement

--- inc/Invoice_Customizations.php
+++ inc/Invoice_Customizations.php
@@ -4,6 +4,25 @@
 class Invoice_Customizations extends SI_Controller {
 
 	public static function init() {
+		add_filter( 'gettext', array( __CLASS__, 'change_tva' ), 100, 3 );
+		add_filter( 'si_line_item_totals', array( __CLASS__, 'modify_line_item_totals_text' ), 100 );
+	}
 
+	public static function modify_line_item_totals_text( $totals ) {
+		foreach ( $totals as $key => $total ) {
+			$totals[ $key ]['label'] = str_replace( 'Tax:', 'VAT:', $total['label'] );
+		}
+		return $totals;
+	}
+
+	public static function change_tva( $translations, $text, $domain ) {
+		if ( 'sprout-invoices' === $domain ) {
+			// Change "Description"
+			if ( 'TVA' === $text ) {
+				return 'VAT';
+			}
+			// add more conditions, changing as many strings as you'd like
+		}
+		return $translations;
 	}
 }
